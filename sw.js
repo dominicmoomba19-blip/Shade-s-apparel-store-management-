2a5796105d057723c30cec1385fb123143f593a6
dominicmoomba19-blip <dominicmoomba19@gmail.com>
2026-05-21 11:10:47 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'shades-apparel-v1';
+const ASSETS = [
+  './fintrack.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-apple.png'
+];
+
+// Install: cache all core assets
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+// Activate: delete old caches
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Fetch: serve from cache, fall back to network
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
